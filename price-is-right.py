d777284491beda96103eecdc55616bbcd757e47b
Ryan Paredes <ryan.paredes@yahoo.com>
2023-06-16 22:25:38 -0700

    Adding prompts

--- price-is-right.py
+++ price-is-right.py
@@ -3,17 +3,18 @@
 itemPrice = 82
 prompt = "What is the price of the item: "
 guess = int(input(prompt))
+nameContestant = "Ryan"
 
 def getWinner(guess):
   if guess == itemPrice :
     print("Your guess of {0} is a great guess! You're spot on. \n Contestant is a Winner!".format(guess))
   elif guess > itemPrice :
     print("Your guess of {0} is over the price. Contestant is out of the game".format(guess))
-  elif guess < itemPrice:
+  elif guess < itemPrice :
     print("Your guess of {0} is under the price. Contestant still in game".format(guess))
 
 try:
   print("\n")
   getWinner(guess)
-except ValueError as error:
+except ValueError as error :
   print('Oops, you did not enter a number')
